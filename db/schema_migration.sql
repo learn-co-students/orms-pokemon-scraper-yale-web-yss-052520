322895a591f088b958e4407afc423e9df812f554
davidyu7 <david.yu@yale.edu>
2020-06-04 18:11:14 -0400

    Done.

--- db/schema_migration.sql
+++ db/schema_migration.sql
@@ -1,2 +1,14 @@
 -- Write your sql commands to create your db schema here.
 -- It should create a table in a new table in the db/ directory.
+
+-- schema_migration.sql
+--   creates pokemon table (FAILED - 1)
+--   has an autoincrementing id column (FAILED - 2)
+--   has a name column (FAILED - 3)
+--   has a type column (FAILED - 4)
+
+CREATE TABLE pokemon (
+    id INTEGER PRIMARY KEY,
+    name TEXT,
+    type TEXT
+);
