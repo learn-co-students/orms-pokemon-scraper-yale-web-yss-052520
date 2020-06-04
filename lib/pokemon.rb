fd5b670c127f320d9bc9131c2de9ede17164484f
Drew Beckmen <drew.beckmen@yale.edu>
2020-06-04 14:33:44 -0400

    updated documentation to clarify what SQL query is returning

--- lib/pokemon.rb
+++ lib/pokemon.rb
@@ -8,14 +8,21 @@ class Pokemon
         @db = db 
     end
     
+    # Saving a Pokemon to database by storing their name and type!
     def self.save(name, type, db)
         db.execute("INSERT INTO pokemon (name,type) VALUES (?,?)", name, type) 
     end
 
+    # Here, we are transforming a unique pokemon stored in the database back into 
+    # a Pokemon object!
     def self.find(id , db)
-        ary = db.execute("SELECT * FROM pokemon WHERE id = ?", id)
-        name = ary[0][1]
-        type = ary[0][2]
+        # Since id is unique, this will return a single row from the pokemon table 
+        # The row is in matrix form (2D) array, so we index 0th element to get the array 
+        # that contains the attributes of the Pokemon. 
+        arr = db.execute("SELECT * FROM pokemon WHERE id = ?", id)
+        new_pokemon = arr[0]
+        name = new_pokemon[1]   #name is 2nd element after id (2nd column)
+        type = new_pokemon[2]   #name is in the 3rd column
         Pokemon.new(id: id ,name: name,type: type,db: db)
     end
 
