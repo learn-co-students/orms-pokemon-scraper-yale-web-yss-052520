357d8926f5a8f422c4a248600ba9e49a68c8b0ea
KalebGz <kaleb.zeray@gmail.com>
2020-06-07 03:06:35 -0400

    Done.

--- lib/pokemon.rb
+++ lib/pokemon.rb
@@ -1,2 +1,21 @@
+require 'sqlite3'
+require 'pry'
 class Pokemon
+    attr_accessor :id, :name, :type, :db
+    def initialize(id:, name:, type:, db:)
+        @id = id
+        @name = name
+        @type = type
+        @db = db
+    end
+
+    def self.save(name, type, db)
+        db.execute('INSERT INTO pokemon (name, type) VALUES (?, ?)', name, type) 
+    end
+
+    def self.find(id, db)
+        poke = db.execute('SELECT * from pokemon WHERE id = ?', id).first
+        # binding.pry
+        new(id:poke[0], name:poke[1], type:poke[2], db:db)
+    end
 end
